28726f21f7eef0b669993e573fc83e4e6c67b121
maxbildner <mbildner@icloud.com>
2021-09-30 11:51:26 -0400

    write front-end components for interacting with lottery contract, update notes/slides

--- section4-interactive-front-ends/lottery-react/src/App.js
+++ section4-interactive-front-ends/lottery-react/src/App.js
@@ -3,29 +3,90 @@ import "./App.css";
 import React from "react";
 import web3 from "./web3";
 
+// import local instance of contract (only exists on client browser)
+import lottery from "./lottery";
+
 class App extends React.Component {
+  // use ES2016 sytnax, instead of constructor method
+  state = { manager: "", players: [], balance: "", value: "" };
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { manager: "" };
+  // }
+
+  // runs once after first render
+  async componentDidMount() {
+    // get info from our local contract instance
+    const manager = await lottery.methods.manager().call();
+    // - NOTE whenever we're using the provider from metamask,
+    //   we don't have to specify where the account is from in the call field
+    //   default will be first account we are signed into in metamask
+    // - manager = the ethereum address of the user who created the local contract instance
+
+    const players = await lottery.methods.getPlayers().call();
+
+    // get ethereum balance of smart contract
+    let balance = await web3.eth.getBalance(lottery.options.address);
+    // - pass in eth address of contract that we want to get the balance of
+    // - web3.eth.getBalnce => returns an Obj (not a number!),
+    // - Obj has balance in Wei
+
+    // convert balance from Wei to Ether
+    balance = web3.utils.fromWei(balance, "ether");
+
+    this.setState({ manager, players, balance });
+  }
+
+  onClickEnter = async (event) => {
+    // prevent classic way of form submission
+    event.preventDefault();
+
+    // get list of all eth account addresses
+    const accounts = await web3.eth.getAccounts();
+    // => returns a promise that resolves to a list of accounts
+
+    // send transaction to the network (could take 15-30 seconds)
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      // - we'll assume that the first account in the array is the one
+      //   sending the transaction
+      value: web3.utils.toWei(this.state.value, "ether"),
+      // - amount of money we want to enter into the lottery contract
+    });
+  };
+
   render() {
     // console.log(web3.version); //=> 1.5.3
 
     // log ethereum address to console
     // web3.eth.getAccounts().then(console.log);
+    // - ? whey async await not needed above?
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          There are currently {this.state.players.length} competing to win&nbsp;
+          {this.state.balance} ether!
+        </p>
+
+        {/* add horizontal divider for style */}
+        <hr />
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              // ? why below needed?
+              // value={this.state.value}
+              onChange={(event) => this.setState({ value: event.target.value })}
+            />
+            {/* send a transaction to the enter function on our lottery contract  */}
+            <button onSubmit={this.onClickEnter}>Enter</button>
+          </div>
+        </form>
       </div>
     );
   }
---
+++ section4-interactive-front-ends/lottery-react/src/lottery.js
@@ -0,0 +1,62 @@
+import web3 from "./web3";
+
+// address of deployed smart contract
+const address = "0x0b77c2D0227DdDEef02b93B92881DcDBCaAb77d7";
+
+// ABI (Application Binary Interface) of smart contract. (1 of the outputs of compiling .sol file)
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+// create and export local instance of the contract (only exists on client browser)
+export default new web3.eth.Contract(abi, address);
